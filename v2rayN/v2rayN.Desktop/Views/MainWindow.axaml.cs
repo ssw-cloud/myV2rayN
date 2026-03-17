dfe92ed7aa5b5e0c653b808afff499dfdfd432cb
饶梓阳 <raoziyang@MacBook-Air.local>
2026-03-17 19:17:26 +0800

    Remove promotion entry and add release packaging

--- v2rayN/v2rayN.Desktop/Views/MainWindow.axaml.cs
+++ v2rayN/v2rayN.Desktop/Views/MainWindow.axaml.cs
@@ -23,7 +23,6 @@ public partial class MainWindow : WindowBase<MainWindowViewModel>
 
         KeyDown += MainWindow_KeyDown;
         menuSettingsSetUWP.Click += MenuSettingsSetUWP_Click;
-        menuPromotion.Click += MenuPromotion_Click;
         menuCheckUpdate.Click += MenuCheckUpdate_Click;
         menuBackupAndRestore.Click += MenuBackupAndRestore_Click;
         menuClose.Click += MenuClose_Click;
@@ -312,11 +311,6 @@ public partial class MainWindow : WindowBase<MainWindowViewModel>
                 ViewModel?.Reload();
             }
         }
-    }
-
-    private void MenuPromotion_Click(object? sender, RoutedEventArgs e)
-    {
-        ProcUtils.ProcessStart($"{Utils.Base64Decode(Global.PromotionUrl)}?t={DateTime.Now.Ticks}");
     }
 
     private void MenuSettingsSetUWP_Click(object? sender, RoutedEventArgs e)
